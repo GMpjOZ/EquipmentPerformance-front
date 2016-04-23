c53cd76a2aa2faaf289f6ed562e504aaaded8396
KOI <1428757462@qq.com>
2016-04-23 19:23:57 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # EquipmentPerformance-front
+
+##安装Node.js
+
+https://nodejs.org/en/download/  
+
+Windows Installer (.msi)   32-bit/64-bit
+
+###检查一下node和npm版本
+
+~>node -v
+
+v4.2.6
+
+~>npm -v
+
+v3.5.3
+
+~>cnpm -v
+
+v4.2.0
+
+tip:
+
+~>npm install -g cnpm --registry=https://registry.npm.taobao.org
+
+npm很慢的话用上面的命令换源，换源以后要用cnpm命令
+
+
+##配置gulp
+
+全局安装gulp和http-server
+
+~>npm install -g gulp http-server
+
+进入项目：
+
+~>npm install gulp-imagemin gulp-sass gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-clean tiny-lr browser-sync gulp-livereload gulp-jslint --save-dev
+
+~>gulp
+
+~>gulp watch
+
+~>http-server
+
+##安装livereload插件
+
+安装好之后导入项目，点击actions+files,打开 compile sass ... 的按钮
+
+在浏览器工具栏上，Enable LiveReload,图标中间变成黄色就OK了
+
+（其实实时更新的挺慢的，还不如自己F5）
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var uglify  = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat  = require('gulp-concat');
+var clean = require('gulp-clean');
+var tinylr = require('tiny-lr');
+var server = tinylr();
+var browserSync = require('browser-sync');
+var port = 35729;
+var livereload = require('gulp-livereload');
+
+var jslint = require('gulp-jslint');
+
+gulp.task('jslint', function () {
+    return gulp.src([
+        './src./js/*.js'
+    ]).pipe(jslint({
+        node: true,
+        nomen: true,
+        sloppy: true,
+        plusplus: true,
+        unparam: true,
+        stupid: true
+    }));
+});
+gulp.task('default', ['jslint']);
+
+gulp.task('html', function() {
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist/'))
+        .pipe(livereload(server))
+});
+
+gulp.task('css', function () {
+  	gulp.src('./src/scss/*.scss')
+    	.pipe(sass.sync().on('error', sass.logError))
+    	.pipe(gulp.dest('./css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(livereload(server))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('images', function(){
+    gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(livereload(server))
+        .pipe(gulp.dest('./dist/images'));
+})
+
+gulp.task('fonts', function () {
+	gulp.src('./src/fonts/*')
+		.pipe(gulp.dest('./dist/fonts'))
+});
+
+gulp.task('js', function () {
+    gulp.src('./src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(livereload(server))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('serve', function() {
+    browserSync({
+        files: "**",
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./src/*.html',['html']);
+    gulp.watch('./src/scss/*/*.scss',['css']);
+    gulp.watch('./src/images/**/*',['images']);
+    gulp.watch('./src/js/*.js',['js']);
+    browserSync.reload();
+});
+
+gulp.task('clean', function() {
+    gulp.src(['./dist/css', './dist/js', './dist/images', '.dist/fonts'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function(){
+    gulp.start('html','css','images','js', 'fonts');
+});
+
+gulp.task('watch',function(){
+	livereload.listen()
+	gulp.watch('./src/*.html',['html']);
+	gulp.watch('./src/scss/*/*.scss',['css']);
+	gulp.watch('./src/images/**/*',['images']);
+	gulp.watch('./src/js/*.js',['js']);
+});
