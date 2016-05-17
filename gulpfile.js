c53a1fde312fc6547c049da88f8e03ae58588265
KOI <1428757462@qq.com>
2016-05-17 20:09:10 +0800

    不用dist和css目录,所有文件都放在src里

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,6 @@ var port = 35729;
 var livereload = require('gulp-livereload');
 
 
-gulp.task('clean', function() {
-    gulp.src(['./dist/css', './dist/js'], {read: false})
-        .pipe(clean());
-});
-
-gulp.task('html', function() {
-    gulp.src('./src/*.html')
-        .pipe(gulp.dest('./dist/'))
-        .pipe(livereload(server))
-});
 
 gulp.task('css', function () {
   	gulp.src('./src/scss/*.scss')
@@ -31,49 +21,28 @@ gulp.task('css', function () {
         .pipe(rename({ suffix: '.min' }))
         .pipe(cleancss())
         .pipe(livereload(server))
-        .pipe(gulp.dest('./css'))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./src/css'))
 });
 
-gulp.task('fonts', function () {
-	gulp.src('./src/fonts/*')
-		.pipe(gulp.dest('./dist/fonts'))
-});
 
 gulp.task('js', function () {
     gulp.src('./src/js/*.js')
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(concat('app.js'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(livereload(server))
-        .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('serve', function() {
-    browserSync({
-        files: "**",
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch('./src/*.html',['html']);
-    gulp.watch('./src/scss/*/*.scss',['css']);
-    gulp.watch('./src/images/**/*',['images']);
-    gulp.watch('./src/js/*.js',['js']);
-    browserSync.reload();
+        .pipe(gulp.dest('./src/js'));
 });
 
 
-
-gulp.task('default', ['clean'], function(){
-    gulp.start('html','css','js', 'fonts');
+gulp.task('default', function(){
+    gulp.start('css','js');
 });
 
 gulp.task('watch',function(){
 	livereload.listen()
 	gulp.watch('./src/*.html',['html']);
 	gulp.watch('./src/scss/*/*.scss',['css']);
-	gulp.watch('./src/images/**/*',['images']);
+	gulp.watch('./src/img/**/*',['img']);
 	gulp.watch('./src/js/*.js',['js']);
 });
